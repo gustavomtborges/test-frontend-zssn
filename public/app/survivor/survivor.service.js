f003e9cbfb85db1fd6caa2ba7e6fb8fc768d4203
Gustavo M T Borges <gustavo.mtborges@gmail.com>
2017-01-24 23:04:09 -0200

    added create survivor function and docs

--- public/app/survivor/survivor.service.js
+++ public/app/survivor/survivor.service.js
@@ -9,9 +9,39 @@
     var service = this;
 
     service.fetch = fetch;
+    service.create = create;
 
+    /**
+     * fetch all survivors
+     * 
+     */
     function fetch() {
       return $http.get(ZSSN_API + '/people.json');
+    }
+
+    /**
+     * create a survivor
+     * 
+     */
+    function create(survivor) {
+      return $http({
+        method: 'POST',
+        url: ZSSN_API + '/people.json',
+        headers: { 'Content-Type': 'application/x-www-form-urlencoded', 'Accept': 'application/json' },
+        transformRequest: function (obj) {
+          var str = [];
+          for (var p in obj)
+            str.push(encodeURIComponent(p) + '=' + encodeURIComponent(obj[p]));
+          return str.join('&');
+        },
+        data: {
+          'person[name]': survivor.name,
+          'person[age]': survivor.age,
+          'person[gender]': survivor.gender,
+          'person[lonlat]': survivor.lonlat,
+          items: survivor.items
+        }
+      });
     }
   }
 })();
